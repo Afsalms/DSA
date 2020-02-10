243a367c13d2b4f9831daba9c742cc6d6dfd00b3
qbuser <afsals@qburst.com>
2020-02-10 08:45:52 +0530

    Completed the linked list functionalities

--- linked_list.py
+++ linked_list.py
@@ -27,15 +27,24 @@ class LinkedList:
             current_node = current_node.next
         return text[:-2]
 
+    def __len__(self):
+        size = 0
+        current_node = self.head
+        while current_node:
+            size += 1
+            current_node = current_node.next
+
+        return size
+
     def add_item(self, item):
         if not isinstance(item, Node):
             item = Node(item)
         if not self.head:
             self.head = item
        	    self.tail = item
-            self.head.next = item
-        self.tail.next = item
-        self.tail = item
+        else:
+            self.tail.next = item
+            self.tail = item
 
     def search_item(self, value):
         is_found = False
@@ -52,6 +61,29 @@ class LinkedList:
         else:
             return -1, None
 
+    def add_to_start(self, item):
+        if not isinstance(item, Node):
+            item = Node(item)
+        if not self.head:
+            self.head = item
+        else:
+            item.next = self.head
+            self.head = item
+
+    def remove_from_index(self, remove_index):
+        if remove_index > len(self):
+            print("Index out of bound")
+            return -1
+        index = 0
+        previous = None
+        current_node = self.head
+        while index != remove_index:
+            previous = current_node
+            current_node = current_node.next
+            index += 1
+        previous.next = current_node.next
+        del(current_node)
+
 a = Node(1)
 b = Node(2)
 c = Node(3)
@@ -64,11 +96,28 @@ ll.add_item(b)
 ll.add_item(c)
 ll.add_item(10)
 ll.add_item(50)
+ll.add_item(60)
+ll.add_to_start(-1)
+ll.add_to_start(-5)
 
-#print(ll)
+print(ll)
 
 
 index, current_node = ll.search_item(10)
-print(index, current_node, current_node.next, current_node.next.next)
+print(index, current_node)
 index, current_node = ll.search_item(100)
 print(index, current_node)
+
+
+print(len(ll))
+
+
+ll.remove_from_index(10)
+
+ll.remove_from_index(1)
+
+print(ll)
+
+ll.remove_from_index(5)
+
+print(ll)
