df79f7266af2741b879888dca01fe11f2247b13d
qbuser <afsals@qburst.com>
2020-03-06 15:16:29 +0530

    Completed the prefix convertion

--- infix_to_machine_format.py
+++ infix_to_machine_format.py
@@ -7,7 +7,6 @@ def convert_infix_postfix(string):
     postfix_string = ""
     operators = ['+', '-', '*', '/']
     for item in string:
-        print("item: ", item)
         if item == "(":
             stack.insert(0, item)
         elif item == ")":
@@ -21,15 +20,35 @@ def convert_infix_postfix(string):
             stack.insert(0, item)
         else:
             postfix_string += item
-        print("iteration ", stack)
-        print(".....................................")
     return postfix_string
 
 
+def convert_infix_prefix(string):
+    stack = []
+    prefix_string = ""
+    operators = ['+', '-', '*', '/']
+    for item in reversed(string):
+        print("item ", item)
+        if item == ")":
+            stack.insert(0, item)
+        elif item == "(":
+            while stack and (i:= stack.pop(0)) != ")":
+               prefix_string += i
+        elif item in operators:
+            if item in ['*', '/'] and stack and stack[0] in operators:
+                while stack and (i:= stack.pop(0)) != ")":
+                    stack.insert(0, ")")
+                    prefix_string += i
+            stack.insert(0, item)
+        else:
+            prefix_string += item
+    return prefix_string[::-1]
 
 
-infix_string = "(A*(B+(C/D)))"
+
+infix_string = input("Enter the infix expression")
 
 a = convert_infix_postfix(infix_string)
-
-print("Result: ",a)
+b = convert_infix_prefix(infix_string)
+print("postfix: ",a)
+print("prefix: ", b)
