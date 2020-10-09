1bfea6dfb7da311e1b288a010c2ab36595d2f48c
Afsalms <afsalms93.mec@gmail.com>
2020-10-09 08:46:14 +0530

    moved to sperate

---
+++ PRACTICE_QUESTIONS/STACK_AND_QUEUE/stack_sort.py
@@ -0,0 +1,15 @@
+import random
+
+input_array = list(range(10))
+
+
+random.shuffle(input_array)
+
+print(input_array)
+
+
+
+while input_array:
+    tmp = input_array[0] ## peek operation
+
+    while a:= input_array.pop(0)
